90b1ad07780145abbaa6c41ac0ca9b096c82c390
Muhamad Ali Khan <ali7847khan@gmail.com>
2024-02-03 09:55:04 +0500

    first commit

--- src/app/authentication/login/login.page.ts
+++ src/app/authentication/login/login.page.ts
@@ -1,37 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Route, Router } from '@angular/router';
-import { AuthService } from '@auth0/auth0-angular';
-import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
   credentials = this.fb.nonNullable.group({
-    email: ['alikhan7847@gmail.com', Validators.required],
-    password: ['12345', Validators.required]
+    email: ['', Validators.required],
+    password: ['', Validators.required],
   });
-
   constructor(
     private fb: FormBuilder,
-    private AuthService: AuthService,
+    private authService: AuthService,
     private loadingController: LoadingController,
+    private alertController: AlertController,
     private router: Router
-  ){}
-
+  ) {
+    this.authService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        console.log('GOT USER ON LOGIN');
+        this.router.navigateByUrl('/groups', { replaceUrl: true });
+      }
+    });
+  }
   get email() {
     return this.credentials.controls.email;
   }
   get password() {
     return this.credentials.controls.password;
   }
-  ngOnInit() {}
+  async login() {
+    const loading = await this.loadingController.create();
+    await loading.present();
 
-  async login(){
+    this.authService.signIn(this.credentials.getRawValue()).then(async (data) => {
+      await loading.dismiss();
 
+      if (data.error) {
+        this.showAlert('Login failed', data.error.message);
+      }
+    });
   }
+  async forgotPw() {
+    const alert = await this.alertController.create({
+      header: 'Receive a new password',
+      message: 'Please insert your email',
+      inputs: [
+        {
+          type: 'email',
+          name: 'email',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Reset password',
+          handler: async (result) => {
+            const loading = await this.loadingController.create();
+            await loading.present();
+            const { data, error } = await this.authService.sendPwReset(result.email);
+            await loading.dismiss();
 
+            if (error) {
+              this.showAlert('Failed', error.message);
+            } else {
+              this.showAlert('Success', 'Please check your emails for further instructions!');
+            }
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  async getMagicLink() {
+    const alert = await this.alertController.create({
+      header: 'Get a Magic Link',
+      message: 'We will send you a link to magically log in!',
+      inputs: [
+        {
+          type: 'email',
+          name: 'email',
+          value: 'ali7847khan@gmail.com',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Get Magic Link',
+          handler: async (result) => {
+            const loading = await this.loadingController.create();
+            await loading.present();
+            const { data, error } = await this.authService.signInWithEmail(result.email);
+            await loading.dismiss();
+            console.log('after signup: ', data);
+            console.log('after signup error: ', error);
+
+            if (error) {
+              this.showAlert('Failed', error.message);
+            } else {
+              this.showAlert('Success', 'Please check your emails for further instructions!');
+            }
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  async showAlert(title: string, msg: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: msg,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { isPlatform } from '@ionic/angular';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private supabase: SupabaseClient;
+  private currentUser: BehaviorSubject<User | boolean | null> = new BehaviorSubject<User | boolean | null>(null);
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+
+
+    this.loadUser();
+  }
+
+  async loadUser() {
+    if (this.currentUser.value) {
+      console.log('ALREADY GOT USER');
+      return;
+    }
+    const user = await this.supabase.auth.getUser();
+    console.log('🚀 ~ file: auth.service.ts ~ line 33 ~ AuthService ~ loadUser ~ session', user);
+
+    if (user.data.user) {
+      this.currentUser.next(user.data.user);
+    } else {
+      this.currentUser.next(false);
+    }
+  }
+
+  signUp(credentials: { email: any; password: any }) {
+    return this.supabase.auth.signUp(credentials);
+  }
+
+  signIn(credentials: { email: any; password: any }) {
+    return this.supabase.auth.signInWithPassword(credentials);
+  }
+
+  signInWithEmail(email: string) {
+    const redirectTo = isPlatform('capacitor') ? 'supachat://login' : `${window.location.origin}/groups`;
+    console.log('set redirect: ', redirectTo);
+
+    return this.supabase.auth.signInWithOtp({ email, options: { emailRedirectTo: redirectTo } });
+  }
+
+  sendPwReset(email: string) {
+    return this.supabase.auth.resetPasswordForEmail(email);
+  }
+
+  async signOut() {
+    await this.supabase.auth.signOut();
+    this.router.navigateByUrl('/', { replaceUrl: true });
+  }
+
+  getCurrentUser(): Observable<User | boolean | null> {
+    return this.currentUser.asObservable();
+  }
+
+  getCurrentUserId(): string | null{
+    if (this.currentUser.value) {
+      return (this.currentUser.value as User).id;
+    } else {
+      return null;
+    }
+  }
+
+  async setSession(access_token: any, refresh_token: any) {
+    return this.supabase.auth.setSession({ access_token, refresh_token });
+  }
 }
